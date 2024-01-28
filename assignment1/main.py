3beac5e6d209bc66cb2a8021bb7708d594f83c51
William L. Thomson Jr. <wlt@o-sinc.com>
2024-01-28 13:37:00 -0500

    assigment1/main.py: Completed initial graph creation with edges & drawing
    - Resized image to 1024 x 1024 for halving, reduces pixellation to draw at
      2x the desired size, and halve it before saving.
    - Increase node size to radius 20
    - Modified font to be Arial font at the node radius size
    - Complete adding proper edges to nodes from file

--- assignment1/main.py
+++ assignment1/main.py
@@ -1,48 +1,72 @@
 #!/usr/bin/python3
 
 from graph import Graph,Vertex
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 graph = Graph()
 image_file = "graph.jpg"
 
 # bounds for the graph image
 x_lower = 0
-x_upper = 100
+x_upper = 1024
 y_lower = 0
-y_upper = 100
+y_upper = 1024
 
 # dimensions of graph image
 width = x_upper - x_lower
 height = y_upper - y_lower
-node_radius = 2
+node_radius = 20
 
 # color constants for PIL
 gray = (100, 100, 100)
 black = (0, 0, 0)
 red = (255, 0, 0)
 green = (0,128,0)
+pink = (255, 105, 180)
 purple = (153, 119, 187)
 
 # create empty PIL image to draw on in memory, image saved at end
 image = Image.new("RGB", (width, height), black)
 draw = ImageDraw.Draw(image)
+font = ImageFont.truetype("arial.ttf", node_radius)
 
 # read coords and add vertices to graph
 with open("coords.txt", "r") as file_coords:
     for line in file_coords :
         values = line.split()
-        print("vertex x%f y%f" % (float(values[1]), float(values[2])))
         vertex = Vertex(int(values[0]), (float(values[1]), float(values[2])))
         # add to graph
         graph.add_vertex(vertex)
-        # draw
+        # draw node with id
         x1 = int(vertex.coords[0]) - node_radius
         y1 = int(vertex.coords[1]) - node_radius
         x2 = int(vertex.coords[0]) + node_radius
         y2 = int(vertex.coords[1]) + node_radius
         draw.ellipse((x1, y1, x2, y2), purple)
+        draw.text((int(vertex.coords[0]) - node_radius/4, int(vertex.coords[1]) - node_radius/2),
+                  str(vertex.id), font=font)
 
+# read graph edges
+with open("graph.txt", "r") as file_graph:
+    for line in file_graph :
+        ids =  [float(i) for i in line.split()]
+        ids_len = len(ids)
+        # first id is me rest are my edges
+        my_id = int(ids[0])
+        vertex = graph.vertices[my_id]
+
+        # go through remaining ids on line after the first
+        for i in range(1,ids_len):
+            neighbor = graph.vertices[ids[i]]
+            if not graph.has_edge(vertex, neighbor) :
+                print("adding edge from %d to %d" % (my_id , ids[i]))
+                graph.add_edge(vertex, neighbor)
+                draw.line([vertex.coords, neighbor.coords], gray, width=2)
+
+    file_graph.close()
+
+# scale down to reduce pixelation
+image = image.resize((width // 2, height // 2), resample=Image.LANCZOS)
 image.save(image_file)
 
 print("Graphs image written to: %s" % image_file)
