302976df7ecf1db00418ac082b50974bbc09e937
William L. Thomson Jr. <wlt@o-sinc.com>
2024-03-24 19:43:16 -0400

    assignment3/main.py: Added constants for directions

--- assignment3/main.py
+++ assignment3/main.py
@@ -1,4 +1,9 @@
 
+# direction constants
+UP = 0
+DOWN = 1
+LEFT = 2
+RIGHT = 3
 
 class ValueIteration():
     """
@@ -8,8 +13,8 @@ class ValueIteration():
     def __init__(self, mdp, maxError=2):
         """
         :param mdp          a MDP with states S, actions A(s), transition model P(s'|s,a),
-                            rewards R(s,a,s'), discount δ/delta
-        :param maxError     the maximum error allowed in the utility of any state
+                            rewards R(s,a,s'), discount γ/gamma
+        :param maxError     the maximum error/ε allowed in the utility of any state
 
         :return U           a policy, vector of utility values for each state
         """
@@ -32,7 +37,9 @@ class ValueIteration():
             if maxChange <= maxError * (1 - delta) / delta:
                 return U
 
-    def qValue(self,mdp, s, a, U):
+    def qValue(self,mdp, state, action, U):
+
+        # sum(P(state_prime|state, action)(self.reward(state, action) + self.gamma * U[state_prime]))
 
         return
 
@@ -50,19 +57,19 @@ class ValueIteration():
         x, y = state
 
         # move up, if action up and y > 0
-        if action == 0 and y > 0:
+        if action == UP and y > 0:
             reward = self.STATES[x][y-1]
 
         # move down, if action down and y < dim
-        elif action == 1 and y < self.dim:
+        elif action == DOWN and y < self.dim:
             reward = self.STATES[x][y+1]
 
         # move left, if action left and x > 0
-        elif action == 2 and x > 0:
+        elif action == LEFT and x > 0:
             reward = self.STATES[x-1][y]
 
         # move right, if action right and x < dim
-        elif action == 3 and x < self.dim:
+        elif action == RIGHT and x < self.dim:
             reward = self.STATES[x+1][y]
 
         return reward
