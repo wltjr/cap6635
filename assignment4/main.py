9bd46cdc533dae90345307b0b5727d2a22f702cc
William L. Thomson Jr. <wlt@o-sinc.com>
2024-04-21 01:46:41 -0400

    assignment4/main.py: Create subplots with error bars & legend

--- assignment4/main.py
+++ assignment4/main.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from sklearn import svm
 from sklearn.datasets import load_iris, load_wine
 from sklearn.metrics import accuracy_score, precision_score, recall_score
@@ -6,12 +8,11 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 
 
-def classifyData(name, dataset):
+def classifyData(name, dataset, row):
     X = dataset.data
     y = dataset.target
     metrics = [[0 for iterations in range(10)] for algo in range(3)]
 
-    print(name)
     for i in range(10):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
         metrics[0][i] = runAlgo("Decision Tree",
@@ -24,16 +25,24 @@ def classifyData(name, dataset):
                                 svm.SVC(kernel="linear", C=0.01),
                                 X_train, X_test, y_train, y_test)
 
-    for i in range(3):
-        min_, max_, avg_ = getMinMaxAvg(metrics[i], 0)
-        print("Accuracy   Min %.2f,  Avg %.2f,  Max %.2f" %
-              (min_, max_, avg_))
-        min_, max_, avg_ = getMinMaxAvg(metrics[i], 1)
-        print("Precision  Min %.2f,  Avg %.2f,  Max %.2f" %
-              (min_, max_, avg_))
-        min_, max_, avg_ = getMinMaxAvg(metrics[i], 2)
-        print("Recall     Min %.2f,  Avg %.2f,  Max %.2f" %
-              (min_, max_, avg_))
+    for i, metric in enumerate(["Accuracy", "Precision", "Recall"]):
+        ax = plt.subplot(2, 3, i + row)
+        ax.set_title(name + " Dataset " + metric)
+        plt.xticks([0,1,2], ["Decision Tree","Naive Bayes","SVM"])
+        plotBar(metrics[0], i, 0)
+        plotBar(metrics[1], i, 1)
+        plotBar(metrics[2], i, 2)
+
+
+def plotBar(data, index, x):
+    min_, max_, avg_ = getMinMaxAvg(data, index)
+    plt.errorbar(x, avg_, fmt='ob', lw=3)
+    plt.text(x + 0.1, avg_, round(avg_, 2))
+    plt.errorbar(x, min_, fmt='_r', lw=3)
+    plt.text(x + 0.1, min_, round(min_, 2))
+    plt.errorbar(x, max_, fmt='_g', lw=3)
+    plt.text(x + 0.1, max_, round(max_, 2))
+    plt.errorbar(x, avg_, max_ - min_, fmt='.k', lw=1)
 
 
 def getMinMaxAvg(data, index):
@@ -52,7 +61,10 @@ def runAlgo(name, algo, X_train, X_test, y_train, y_test):
     
     return accuracy, precision, recall
 
-
-classifyData("Iris Classification", load_iris())
+plt.subplots(nrows=2, ncols=3, figsize=(11, 7))
+classifyData("Iris", load_iris(), 1)
 print()
-classifyData("Wine Classification", load_wine())
+classifyData("Wine", load_wine(), 4)
+plt.figlegend(["avg", "min", "max"], loc = 'lower center', ncol=5, labelspacing=0.)
+plt.tight_layout(pad=3)
+plt.show()
