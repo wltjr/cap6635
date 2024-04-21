caed2c6e7e49e1082a10b35d7dd5eab4c76fcf8f
William L. Thomson Jr. <wlt@o-sinc.com>
2024-04-20 23:41:05 -0400

    assignment4/main.py: Wrap into loop & print min, max, & avg for each algo

--- assignment4/main.py
+++ assignment4/main.py
@@ -9,12 +9,39 @@ from sklearn.tree import DecisionTreeClassifier
 def classifyData(name, dataset):
     X = dataset.data
     y = dataset.target
+    metrics = [[0 for iterations in range(10)] for algo in range(3)]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
     print(name)
-    accuracy, precision, recall = runAlgo("Decision Tree", DecisionTreeClassifier(), X_train, X_test, y_train, y_test)
-    accuracy, precision, recall = runAlgo("Naive Bayes", GaussianNB(), X_train, X_test, y_train, y_test)
-    accuracy, precision, recall = runAlgo("SVM", svm.SVC(kernel="linear", C=0.01), X_train, X_test, y_train, y_test)
+    for i in range(10):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+        metrics[0][i] = runAlgo("Decision Tree",
+                                DecisionTreeClassifier(),
+                                X_train, X_test, y_train, y_test)
+        metrics[1][i] = runAlgo("Naive Bayes",
+                                GaussianNB(),
+                                X_train, X_test, y_train, y_test)
+        metrics[2][i] = runAlgo("SVM",
+                                svm.SVC(kernel="linear", C=0.01),
+                                X_train, X_test, y_train, y_test)
+
+    for i in range(3):
+        min_, max_, avg_ = getMinMaxAvg(metrics[i], 0)
+        print("Accuracy   Min %.2f,  Avg %.2f,  Max %.2f" %
+              (min_, max_, avg_))
+        min_, max_, avg_ = getMinMaxAvg(metrics[i], 1)
+        print("Precision  Min %.2f,  Avg %.2f,  Max %.2f" %
+              (min_, max_, avg_))
+        min_, max_, avg_ = getMinMaxAvg(metrics[i], 2)
+        print("Recall     Min %.2f,  Avg %.2f,  Max %.2f" %
+              (min_, max_, avg_))
+
+
+def getMinMaxAvg(data, index):
+    min_ = min(data, key = lambda a: a[index])[index]
+    max_ = max(data, key = lambda a: a[index])[index]
+    avg_ = (min_ + max_) / 2
+
+    return min_, max_, avg_
 
 
 def runAlgo(name, algo, X_train, X_test, y_train, y_test):
@@ -22,8 +49,6 @@ def runAlgo(name, algo, X_train, X_test, y_train, y_test):
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred, average='macro')
     recall = recall_score(y_test, y_pred, average='macro')
-    print("%13s, Accuracy %.2f,  Precision %.2f,  Recall %.2f" %
-          (name, accuracy, precision, recall))
     
     return accuracy, precision, recall
 
